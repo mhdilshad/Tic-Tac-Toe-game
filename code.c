7420fa36652d1238dacc522b97b7da442ab890e4
Md Mahfuz Hayder Dilshad <mhdilshad45@gmail.com>
2026-07-10 00:01:19 +0600

    UI rendering

--- code.c
+++ code.c
@@ -1,1 +1,40 @@
+// Configuration & State:>
 
+
+
+//UI Rendering:>
+void draw_board() {
+
+    #ifdef _WIN32
+    #else
+        system("clear");
+    #endif
+
+    printf("\n");
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c  \n", board[1], board[2], board[3]);
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c  \n", board[4], board[5], board[6]);
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c  \n", board[7], board[8], board[9]);
+    printf("     |     |     \n\n");
+}
+
+
+
+
+//Core Logic:>
+
+
+
+
+
+
+
+
+
+
+
+//Main Game Loop:>
